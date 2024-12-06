70a1e02e063098b071e7020681d38d9c3d35446a
frontend <frontend.divine@gmail.com>
2024-12-06 10:06:08 +0530

    changes in database live

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
-const uri = "mongodb://127.0.0.1:27017/templateshub"; // Replace with your MongoDB connection string
+const uri =
+  "mongodb+srv://mauliksolanki2002:Maulik%401742@cluster0.r46zm.mongodb.net/templateshub"; // Replace with your MongoDB connection string
 
 mongoose
-    .connect(uri)
-    .then(() => {
-        console.log("Connected to MongoDB successfully!");
-    })
-    .catch((error) => {
-        console.error("Error connecting to MongoDB:", error);
-    });
+  .connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB successfully!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
